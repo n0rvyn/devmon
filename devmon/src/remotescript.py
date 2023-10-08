76471c486c7119f2c633c291969ed364fde84d6e
n0rvyn <norvyn@norvyn.com>
2023-10-08 12:08:20 +0800

    update

---
+++ devmon/src/remotescript.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2020-2022 by ZHANG ZHIJIE.
+# All rights reserved.
+
+# Created Time: 2023-10-09 09:46
+# Author: ZHANG ZHIJIE
+# Email: norvyn@norvyn.com
+# Git: @n0rvyn
+# File Name: remotescript.py
+# Tools: PyCharm
+
+"""
+---Executing script via SSH ---
+"""
+from .ssh import PySSHClient
+
+
+class RemoteScript(object):
+    def __init__(self):
+        pass
