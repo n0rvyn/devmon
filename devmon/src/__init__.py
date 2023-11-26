97a54576154e1701bd3a192ee6b38e134d600c03
n0rvyn <norvyn@norvyn.com>
2023-11-26 15:14:46 +0800

    update source code files structure

--- devmon/src/__init__.py
+++ devmon/src/__init__.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
-_ROOT_ = os.path.abspath(os.path.dirname(__file__))
-_SRC_ = os.path.abspath(os.path.join(_ROOT_, 'src'))
-
-
-try:
-    from type import (Entry, EntryValue, ArithPosition, Agent, SNMPDetail, SSHDetail,
-                      Version, Case, TheSameCasePart,
-                      CaseUpdatePart, EventType, Point, PointMeta)
-    from core import (SNMP, ColorLogger, entry_to_case, PySSHClient,
-                      PushMsg, MongoDB, CMDB, ContextSNMP, HidePass,
-                      MongoPoint, InfluxDB, read_agents)
-except ModuleNotFoundError:
-    from .type import (Entry, EntryValue, ArithPosition, Agent, SNMPDetail, SSHDetail,
-                       Version, Case, TheSameCasePart,
-                       CaseUpdatePart, EventType, Point, PointMeta)
-    from .core import (SNMP, ColorLogger, entry_to_case, PySSHClient,
-                       PushMsg, MongoDB, CMDB, ContextSNMP, HidePass,
-                       MongoPoint, InfluxDB, read_agents)
+from .agent import Version, Agent, SNMPDetail, SSHDetail, Host
+from .entry import Entry, WaterMark, ArithType, EntryValue, ArithPosition
+from .case import Case, CaseType, TheSameCasePart, CaseUpdatePart
+from .event import EventType
+from .point import PointMeta, Point
+from .read_devlist import read_agents
+from .snmp import SNMP, ContextSNMP
+from .log import ColorLogger
+from .pushmsg import PushMsg
+from .mongo import MongoDB, MongoPoint
+from .cmdb import CMDB
+from .cre_case import entry_to_case
+from .encrypt import HidePass
+from .influx import InfluxDB
+from .ssh import PySSHClient
 
 
 __all__ = [
@@ -45,7 +41,34 @@ __all__ = [
     'InfluxDB',
     'read_agents',
     'ArithPosition',
+    'PySSHClient',
+    'Version',
+    'SNMPDetail',
+    'SSHDetail',
+    'Agent',
+    'Host',
+    'Entry',
+    'EntryValue',
+    'WaterMark',
+    'Case',
+    'CaseType',
+    'ArithType',
+    'ArithPosition',
+    'TheSameCasePart',
+    'CaseUpdatePart',
+    'EventType',
+    'Point',
+    'PointMeta',
+    'SNMP',
+    'ColorLogger',
+    'PushMsg',
+    'MongoDB',
+    'CMDB',
+    'ContextSNMP',
+    'entry_to_case',
+    'HidePass',
+    'MongoPoint',
+    'InfluxDB',
+    'read_agents',
     'PySSHClient'
 ]
-
-
